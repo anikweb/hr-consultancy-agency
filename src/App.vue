18e55630b51883a4ba2061095e27dde9f2c4acc6
anikweb <aniknandi.it@gmail.com>
2024-10-22 22:32:13 +0600

    added breadcrumb

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router"
 import Navbar from '@/components/Navbar.vue'
+import Breadcrumb from "@/components/Breadcrumb.vue";
 </script>
 
 <template>
     <Navbar />
+    <Breadcrumb />
     <RouterView />
 </template>
---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,14 @@
+<script setup>
+const breadCrumbBgColor = "#E1E8FF";
+</script>
+<template>
+    <section class="" :style="`background-color:${breadCrumbBgColor}`">
+        <div class="container py-4 mx-auto">
+            <ul>
+                <li>
+                    Home
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
